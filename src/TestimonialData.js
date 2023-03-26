41a6dcacf18c89ed62c626befffafd1eb88754f7
Eric Macbook <ericji@Erics-MBP.lan1>
2023-03-25 20:35:01 -0500

    minor changes shoutout greg

--- src/TestimonialData.js
+++ src/TestimonialData.js
@@ -1,46 +1,51 @@
 const testimonialData = [
     {
         key: 0,
+        quote: "' Talking to Eric is like talking to a friend, someone who will listen and support your vision '",
+        author: "Client"
+    },
+    {
+        key: 1,
         quote: "' Cool guy, great developer... what more could you ask for!? '",
         author: "Tennis Coach"
     },
     {
-        key: 1,
+        key: 2,
         quote: "' Makes the best banana bread in town '",
         author: "Friend"
     },
     {
-        key: 2,
+        key: 3,
         quote: "' He makes some pretty funny websites '",
         author: "Dad"
     },
     {
-        key: 3,
+        key: 4,
         quote: "' Everyone's biggest cheerleader '",
         author: "Friend"
     },
     {
-        key: 4,
+        key: 5,
         quote: "' If glass half full was a person it'd be Eric '",
         author: "Friend"
     },
     {
-        key: 5,
+        key: 6,
         quote: "' Pretty good at web development, average at minecraft '",
         author: "Friend"
     },
     {
-        key: 6,
+        key: 7,
         quote: "' Number one hype man with a heart of gold '",
         author: "Coworker"
     },
     {
-        key: 7,
+        key: 8,
         quote: "' The websites created by Eric are visually appealing and easy to navigate '",
         author: "Mom"
     },
     {
-        key: 8,
+        key: 9,
         quote: "' Eric is like a breath of fresh air: refreshing and always welcome '",
         author: "Friend"
     }
